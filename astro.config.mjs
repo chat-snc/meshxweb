154f40fff5832c780966660ff2e957441504279d
Chathuranga Bandara <chathurangapayment@gmail.com>
2024-05-10 18:31:47 +0530

    added github ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chat-snc.github.io',
+  base: 'meshxweb',
   integrations: [tailwind()]
 });
